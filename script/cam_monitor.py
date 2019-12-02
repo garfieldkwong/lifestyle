75bacc6a774ad2520ba3e13c6e84591516c6618d
garfieldkwong <garfieldkwong@gmail.com>
2019-12-02 21:24:55 +0800

    Use email server instead of polling gmail.

--- script/cam_monitor.py
+++ script/cam_monitor.py
@@ -1,12 +1,16 @@
+import asyncio
+import email
+import email.header
 import json
 import os
-import time
 import miio
 import re
-from g_service import gmail
-from utils import logger
+from utils import async_helper, logger
 
 DEFAULT_CFG_PATH = 'cfg.json'
+log = logger.create_logger(
+    os.path.splitext(os.path.basename(__file__))[0]
+)
 
 
 def load_config(config_path=None):
@@ -18,48 +22,46 @@ def load_config(config_path=None):
         return cfg_data
 
 
-def run(config=None):
-    """Run"""
-    log = logger.create_logger(
-        os.path.splitext(os.path.basename(__file__))[0]
-    )
-    cfg_data = load_config(config)
+async def _check_email_subject(subject, cfg_data):
+    """Check the email subject"""
     subject_regex = re.compile(cfg_data['subject_regex'])
-
-    gmail_service = gmail.GMail(
-        creds_filename='credentials.json'
-    )
-    while True:
-        log.info('')
-        sleep_duration = 60
-        handled = False
-        msg_infos = gmail_service.list_messages(
-            query='is:unread', labelIds=cfg_data['gmail_lable_id']
+    matched = subject_regex.match(subject)
+    if matched and \
+            matched.group(1) == cfg_data['cam_target'] and \
+            matched.group(2) == cfg_data['cam_offline_indicator']:
+        device = miio.chuangmi_plug.ChuangmiPlug(
+            ip=cfg_data['socket_ip'], token=cfg_data['socket_token']
         )
-        for msg_info in msg_infos:
-            if not handled:
-                msg_data = gmail_service.get_message(msg_info['id'])
-                subject = ''
-                for item in msg_data['payload']['headers']:
-                    if item['name'] == 'subject':
-                        subject = item['value']
-                matched = subject_regex.match(subject)
-                if matched and \
-                   matched.group(1) == cfg_data['cam_target'] and \
-                   matched.group(2) == cfg_data['cam_offline_indicator']:
-                    device = miio.chuangmi_plug.ChuangmiPlug(
-                        ip=cfg_data['socket_ip'], token=cfg_data['socket_token']
-                    )
-                    result = device.off()
+        result = await async_helper.async_call(device.off)
 
-                    log.info('off %s', result)
+        log.info('off %s', result)
 
-                    time.sleep(1)
-                    result = device.on()
-                    log.info('on %s', result)
-                    sleep_duration = 3600
-            gmail_service.mark_as_read(msg_info['id'])
-        time.sleep(sleep_duration)
+        await asyncio.sleep(1)
+
+        result = await async_helper.async_call(device.on)
+        log.info('on %s', result)
+        return True
+    return False
+
+
+class EmailHandler(object):
+    """Email handler"""
+    def __init__(self, cfg_path):
+        super().__init__()
+        self._cfg_data = load_config(cfg_path)
+
+    async def handler_RCPT(
+            self, server, session, envelope, address, rcpt_options
+    ):
+        return '250 OK'
+
+    async def handle_DATA(self, server, session, envelope):
+        msg = email.message_from_bytes(envelope.content)
+        sub_bytes, encoding = email.header.decode_header(msg['subject'])[0]
+        subject = sub_bytes.decode(encoding)
+        log.info('Received email with subject: %s', subject)
+        await _check_email_subject(subject, self._cfg_data)
+        return '250 Message accepted for delivery'
 
 
 if __name__ == '__main__':
@@ -68,4 +70,8 @@ if __name__ == '__main__':
     arg_parser.add_argument('--config')
     args = arg_parser.parse_args()
 
-    run(args.config)
+    from aiosmtpd.controller import Controller
+    controller = Controller(EmailHandler(args.config), hostname='0.0.0.0')
+    controller.start()
+
+    asyncio.get_event_loop().run_forever()
